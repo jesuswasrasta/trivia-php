da8936298b280eb56cd9a40d00bbc0b96965538d
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2020-04-15 06:56:41 +0200

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Trivia
+
+Nothing more than https://github.com/jbrains/trivia.  
+I just added some other _obstacles_:  
+* dealing with filesystem
+* static classes
+* ...
---
+++ tests/GameTest.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+//use Game;
+use PHPUnit\Framework\TestCase;
+
+require_once(__DIR__.'/../src/Game.php');
+
+class GameTest extends TestCase
+{
+    /** @test */
+    public function
+    it_does_something()
+    {
+        $this->fail('This test has not been implemented yet.');
+    }
+}
